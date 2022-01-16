f97d47abc69283d25aefd5b8acb7241520847b14
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-01-16 21:46:03 +0530

    Create Pascal's Triangle2.cpp

---
+++ Pascal's Triangle2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> getRow(int row) 
+    {
+        if(row==0)
+            return {1};
+        int n=row;
+        vector<vector<int>>res;
+        vector<int>v;
+        if(n>=1)  
+       {
+           v.push_back(1);
+           res.push_back(v);  
+           // return res;
+       }
+        
+       for(int a=1;a<=n;a++)
+       {
+           vector<int>v1;
+           v1.push_back(1);
+            for(int b=1;b<a;b++)//a=3 b=1
+            {
+                int val=res[a-1][b-1]+res[a-1][b];
+                v1.push_back(val);
+            }
+           v1.push_back(1);
+           res.push_back(v1);
+       }
+       return res[n]; 
+    }
+};
