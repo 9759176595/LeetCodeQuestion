4a24b1eea93a22a2fdc7328c0f50faa3d0433576
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-01-21 10:21:57 +0530

    Create  Find First Palindromic String in the Array.cpp

---
+++ Find First Palindromic String in the Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPal(string s){
+        int left=0, right=s.length()-1;
+        while(left<right){
+            if(s[left++]!=s[right--])
+                return false;
+        }
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        string ans="";
+        for(int i=0;i<words.size();i++){
+            if(isPal(words[i])){
+                ans=words[i];
+                break;
+            }
+        }
+        return ans;
+    }
+};
