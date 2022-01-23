6426ca538325dc1dfd518639499f924585de9ed1
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-01-23 09:10:55 +0530

    Create Ransom Note.cpp

---
+++ Ransom Note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+      vector<int> magazineHash(26,0);
+         for(int i=0;magazine[i]!='\0';i++)
+             magazineHash[magazine[i]-'a']+=1;
+        
+        for(int i=0;ransomNote[i]!='\0';i++){
+            if(magazineHash[ransomNote[i]-'a']==0)
+                return false;
+            magazineHash[ransomNote[i]-'a']-=1; //using up char will lead to decrementing it by value 1
+        }
+        return 1;
+    }
+};
