40caf4a5ded8708f9e830447d3457413c3e0fa04
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-02-08 14:24:56 +0530

    Create Minimum Cost of Buying Candies With Discount.cpp

---
+++ Minimum Cost of Buying Candies With Discount.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(),cost.end());
+        int n=cost.size();
+        
+        int price=0;
+        int i=n-1;
+        while(i>1){
+            
+            /// 2 candy add kro 3 wale ko skip kro end se
+            // 3rd wala free me mil jayege
+            price+=cost[i--];
+            price+=cost[i--];
+            i--;
+        }
+        while(i>=0){
+            price+=cost[i-1];
+        }
+        return price;
+    }
+};
