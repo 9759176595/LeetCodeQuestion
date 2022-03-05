fd69a0da8410fb14c723fed1e37166bf079e7a41
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-03-05 10:07:20 +0530

    Update Delete and Earn.java

--- Delete and Earn.java
+++ Delete and Earn.java
@@ -2,11 +2,11 @@ class Solution {
     public int deleteAndEarn(int[] nums) {
         int inc=0;
         int exc=0;
-        int count[]=new int[1001];
+        int count[]=new int[10001];
         for(int val: nums){
             count[val]++;
         }
-        for(int i=0;i<=1000;i++){
+        for(int i=0;i<=10000;i++){
             int ni=exc+count[i]*i;
             int ne=Math.max(inc,exc);
             
