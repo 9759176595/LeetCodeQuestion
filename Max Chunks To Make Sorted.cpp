1f367a05b11383ed74b3d0d9ab0358e229d13e40
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-04-01 10:53:48 +0530

    Create Max Chunks To Make Sorted.cpp

---
+++ Max Chunks To Make Sorted.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int maxi=0;
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            maxi=max(arr[i],maxi);
+            if(maxi==i)
+                count++;
+        }
+        return count;
+    }
+};
