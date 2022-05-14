71e357bd76f6c3542049d105defa8ef27d9a2508
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2022-05-14 12:16:50 +0530

    Create H-Index II.cpp

---
+++ H-Index II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+       int n=citations.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(citations[mid]<n-mid)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return n-l;
+        
+        //M2
+        // int i=n-1;
+        // while(i>=0){
+        //     if(citations[i]<n-i) break;
+        //     i--;
+        // }
+        // return n-i-1;
+    }
+};
